df4b09dee3a1036505a48a43609c65c287d4b408
hquief <hquief@partner.auchan.fr>
2025-08-28 11:22:48 +0200

    feat: update front

--- packages/front/src/vite-env.d.ts
+++ packages/front/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly BACK_API_HOST: string;
+  readonly BACK_API_PORT: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv & {
+    readonly MODE: 'development' | 'production' | 'test';
+  };
+}
