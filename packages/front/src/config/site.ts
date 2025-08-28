72371335ce242abbba2770b103ba836973a555d0
hquief <hquief@partner.auchan.fr>
2025-08-28 16:55:59 +0200

    feat: update navbar

--- packages/front/src/config/site.ts
+++ packages/front/src/config/site.ts
@@ -1,73 +1,17 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: 'Vite + HeroUI',
-  description: 'Make beautiful websites regardless of your design experience.',
+  name: 'MTGdle',
+  description: 'Guess Magic: The Gathering card.',
   navItems: [
-    {
-      label: 'Home',
-      href: '/',
-    },
-    {
-      label: 'Docs',
-      href: '/docs',
-    },
-    {
-      label: 'Pricing',
-      href: '/pricing',
-    },
-    {
-      label: 'Blog',
-      href: '/blog',
-    },
-    {
-      label: 'About',
-      href: '/about',
-    },
     {
       label: 'Health',
       href: '/health',
     },
   ],
-  navMenuItems: [
-    {
-      label: 'Profile',
-      href: '/profile',
-    },
-    {
-      label: 'Dashboard',
-      href: '/dashboard',
-    },
-    {
-      label: 'Projects',
-      href: '/projects',
-    },
-    {
-      label: 'Team',
-      href: '/team',
-    },
-    {
-      label: 'Calendar',
-      href: '/calendar',
-    },
-    {
-      label: 'Settings',
-      href: '/settings',
-    },
-    {
-      label: 'Help & Feedback',
-      href: '/help-feedback',
-    },
-    {
-      label: 'Logout',
-      href: '/logout',
-    },
-  ],
+  navMenuItems: [],
   links: {
-    github: 'https://github.com/frontio-ai/heroui',
-    twitter: 'https://twitter.com/hero_ui',
-    docs: 'https://heroui.com',
-    discord: 'https://discord.gg/9b6yyZKmH4',
-    sponsor: 'https://patreon.com/jrgarciadev',
+    github: 'https://github.com/CalibanAngel/mtgdle',
+    scyrfall: 'https://scryfall.com',
   },
 };
